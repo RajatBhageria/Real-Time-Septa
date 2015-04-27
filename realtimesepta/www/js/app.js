dc7839713b4382c6ff9cb79605054a9e1e6287cd
ColinRoberts <colinroberts96@gmail.com>
2015-04-26 20:33:14 -0400

    Bug fix
    fixed auto route to non existing view

--- realtimesepta/www/js/app.js
+++ realtimesepta/www/js/app.js
@@ -56,5 +56,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/map');
 });
